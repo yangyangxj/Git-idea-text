d4c4156f14bebbe8825e72c62f5f9f6d580596d4
yangyang <yy@qq.com>
2022-03-31 21:59:22 +0800

    这是text第2次提交

--- src/test/java/GitText.java
+++ src/test/java/GitText.java
@@ -1,6 +1,6 @@
 public class GitText {
     public static void main(String[] args) {
-        System.out.println("hello gitl");
-        System.out.println("hello git text分支");
+        System.out.println("这是一个测试文件");
+
     }
 }
