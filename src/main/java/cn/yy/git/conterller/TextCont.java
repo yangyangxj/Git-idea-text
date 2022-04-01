4c6ca9f53577a8860735c6bb60b80adf2d0970e3
yangyang <102637696+yangyangxj@users.noreply.github.com>
2022-04-01 17:32:56 +0800

    Update TextCont.java

--- src/main/java/cn/yy/git/conterller/TextCont.java
+++ src/main/java/cn/yy/git/conterller/TextCont.java
@@ -6,5 +6,6 @@ public class TextCont {
         System.out.println("hello git dev");
         System.out.println("hello git text分支");
         System.out.println("这是github上的第一次提交");
+        System.out.println("这是在github网站上的第一次提交");
     }
 }
